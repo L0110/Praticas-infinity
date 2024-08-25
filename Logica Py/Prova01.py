fc98c186eb223e97cf92c63ac97dd1eb2d2f41ac
Filipe Anunciação <lipe_z@outlook.com>
2024-08-25 18:02:03 -0300

    Prova01 0.1
    Finalização da primeira prova, onde é esperada a entrada dos valores correspondentes a base e altura de um retângulo. O código irá processar a os dados, multiplicados entre si, com base na fórmula de área: AREA = BASE X ALTURA.
    O resultado é impresso logo em seguida

---
+++ Logica Py/Prova01.py
@@ -0,0 +1,11 @@
+"""
+Você está criando um programa para calcular a área de um retângulo. O programa deve solicitar ao usuário que 
+forneça a base e a altura do retângulo. Em seguida, o programa deve calcular a área usando a fórmula:
+    Área=Base×Altura
+"""
+
+print("**** Calculo da área de um retângulo ****")
+a = float(input("insira o tamanho da base: "))
+b = float(input("insira o tamanho da altura: "))
+c = a * b
+print(f"A área do retângulo é :{c}")

22f9fa395c76ae1c0e14341bff29e22da1a0f0d2
Filipe Anunciação <lipe_z@outlook.com>
2024-12-24 00:47:14 -0300

    ATV 02

---
+++ Logica Py 2/Aula 01/atv02.py
@@ -0,0 +1,6 @@
+"""Faça a definição de uma lista contendo as vogais.
+Finalmente, utilize o print() para exibir os valores da lista."""
+
+vogais = ['A','E','I','O','U']
+for i in vogais:
+    print(i)

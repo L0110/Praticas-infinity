a35d40de479a99c0f71f9496c7d6f4699efe827e
Filipe Anunciação <lipe_z@outlook.com>
2025-02-21 00:35:15 -0300

    Elaboracao da prova 08

---
+++ Logica Py 2/aula08/Prova/main.py
@@ -0,0 +1,45 @@
+"""
+ Importe o módulo 'os' e use a função 'listdir' para listar todos os arquivos e 
+ pastas presentes no diretório onde o script Python está sendo executado. A função 'listdir' 
+ retornará uma lista contendo os nomes dos arquivos e pastas. Após obter essa lista, exiba cada item 
+ da lista individualmente na saída do console.
+"""
+
+import os
+
+
+#Definicao do caminho
+caminho = 'Logica Py 2'
+conteudo = os.listdir(caminho)
+
+# Listagem de todas as pastas no camimnho
+conteudo_diretorio = os.listdir(caminho)
+print(f"Pastas do diretorio: {caminho}")
+print("-"*60)
+
+for i in conteudo_diretorio:
+    print("- ",i)
+
+
+print("-"*60)
+print(f"Arquivos do diretorio: {caminho}")
+print("-"*60)
+
+#Listagem de todos os arquivos no diretorio e os subdiretorios
+for item in conteudo_diretorio:
+
+    caminho_completo = os.path.join(caminho,item)
+    
+    if os.path.isdir(caminho_completo):
+        #listagem dos subdiretorios
+        subdiretorio_conteudo = os.listdir(caminho_completo)
+        
+        for subitem in subdiretorio_conteudo:
+            
+            if os.path.isfile(os.path.join(caminho_completo,subitem)):
+                
+                print("- ",os.path.join(caminho_completo,subitem))
+                
+            elif os.path.isfile(caminho_completo):
+                #listagem dos arquivos do diretorio principal
+                print("- ",caminho_completo)

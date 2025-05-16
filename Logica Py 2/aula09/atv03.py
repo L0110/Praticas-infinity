662dbe6db31b8678309b2444499aea212d1e29ef
Filipe Anunciação <lipe_z@outlook.com>
2025-05-15 22:32:29 -0300

    Ingresso das atividades Web realizadas

--- Logica Py 2/aula09/atv03.py
+++ Logica Py 2/aula09/atv03.py
@@ -1,14 +1,40 @@
-"""
-O aplicativo deve ser capaz de realizar operações básicas de matemática, como adição, subtração, multiplicação e divisão.
-Implemente uma interface limpa e intuitiva, com botões para os números e operações, além de uma área para exibir o resultado das operações.
-"""
-
 import flet as ft
 
-def principal(janela:ft.Page):
+def principal(janela: ft.Page):
     janela.title = "Calculadora v0.01"
     janela.bgcolor = ft.colors.BLUE_200
 
+    janela.controls = [
+        ft.Row(
+            controls=[ft.Text(value="0", size=40)],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="1", on_click=lambda e: print("1")),
+                ft.ElevatedButton(text="2", on_click=lambda e: print("2")),
+                ft.ElevatedButton(text="3", on_click=lambda e: print("3")),
+                ft.ElevatedButton(text="4", on_click=lambda e: print("4")),
+                ft.ElevatedButton(text="5", on_click=lambda e: print("5")),
+                ft.ElevatedButton(text="6", on_click=lambda e: print("6")),
+                ft.ElevatedButton(text="7", on_click=lambda e: print("7")),
+                ft.ElevatedButton(text="8", on_click=lambda e: print("8")),
+                ft.ElevatedButton(text="9", on_click=lambda e: print("9")),
+                ft.ElevatedButton(text="0", on_click=lambda e: print("0")),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="+", on_click=lambda e: print("+")),
+                ft.ElevatedButton(text="-", on_click=lambda e: print("-")),
+                ft.ElevatedButton(text="*", on_click=lambda e: print("*")),
+                ft.ElevatedButton(text="/", on_click=lambda e: print("/")),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+    ]
+
     janela.update()
-ft.app(principal)
-    
+
+ft.app(target=principal)
